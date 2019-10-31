eb4d1177639c729bf4b07f256fdcd03defe0a09a
amy0529 <499488586@qq.com>
2019-10-31 12:39:52 +0800

    Default Changelist

--- amy_test/001.py
+++ amy_test/001.py
@@ -1,2 +1,28 @@
-i=(1,2,3,4)
-print(i.index(3))
+import sys
+# sys.path[0]='X:\github\source_code\\amy_test\module'
+# print(sys.path)
+from Class_module import FishBox
+
+big_gif_box=FishBox(60,30,40)
+big_gif_box.price=1000
+big_gif_box.amount=20
+big_gif_box.weight=10
+print('大礼盒的体面积是%d立方厘米'%(big_gif_box.volume()))
+print('大礼盒的表面积是%d平方厘米'%big_gif_box.area())
+index=big_gif_box.type.index('大礼盒')
+g_box_num=big_gif_box.countBoxnums(200,index)
+print('200条鱼需要%d只大礼盒'%(g_box_num))
+print(g_box_num)
+print('200条鱼装大礼盒的价值为%d元'%(g_box_num*big_gif_box.price))
+small_gif_box=FishBox(50,20,30)
+small_gif_box.price=500
+small_gif_box.amount=10
+small_gif_box.weight=5
+print('小礼盒的体面积是%d立方厘米'%(small_gif_box.volume()))
+print('小礼盒的表面积是%d平方厘米'%small_gif_box.area())
+index=small_gif_box.type.index('小礼盒')
+print(index)
+small_box_num=big_gif_box.countBoxnums(200,index)
+print(small_box_num)
+print('200条鱼需要%d只大礼盒'%(small_box_num))
+print('200条鱼装小礼盒的价值为%d元'%(small_gif_box.price*small_box_num))
---
+++ amy_test/Class_module.py
@@ -0,0 +1,39 @@
+class Box1():                                   #定义父类Box1
+    '''求立方体的类'''
+    def  __init__(self,length1,width1,height1):
+        self.length=length1
+        self.width=width1
+        self.height=height1
+    def volume(self):
+        return self.length*self.width*self.height
+
+
+class Box2(Box1):                             #继承Box1定义子类Box2
+    def __init__(self,length1,width1,height1): #子类也是__init__开始,可以从父类原样拷贝代码
+        super().__init__(length1,width1,height1) #super实现父类与子类的关联,继承父类的属性
+        self.color='white'                       #增加颜色属性
+        self.material='paper'                    #增加材质属性
+        self.type='fish'                         #增加类型属性
+    def area(self):
+        result=self.length*self.width*2+self.length*self.height*2+self.width*self.height*2
+        return result
+    def volume(self,num=1):                      #重写volume()方法
+         return self.length*self.width*self.height*num
+class FishBox(Box2):
+    def __init__(self,length1,width1,height1):
+        super().__init__(length1,width1,height1)
+        self.price=0
+        self.amount=0
+        self.type=('大礼盒','小礼盒')
+        self.weight=0
+    def countBoxnums(self,fish_nums,f_type_index):
+        if f_type_index==0:
+            self.amount=20
+        else:
+            self.amount=10
+        if fish_nums%self.amount==0:
+            return fish_nums/self.amount
+        else:
+            return fish_nums/self.amount+1
+    def total(self,box_nums,price):
+        return box_nums*price
