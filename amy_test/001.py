f361a813bb208f059d5974e6e53e227cde6c523a
amy0529 <499488586@qq.com>
2019-11-01 10:25:29 +0800

    Default Changelist

--- amy_test/001.py
+++ amy_test/001.py
@@ -1,28 +1,6 @@
-import sys
-# sys.path[0]='X:\github\source_code\\amy_test\module'
-# print(sys.path)
-from Class_module import FishBox
-
-big_gif_box=FishBox(60,30,40)
-big_gif_box.price=1000
-big_gif_box.amount=20
-big_gif_box.weight=10
-print('大礼盒的体面积是%d立方厘米'%(big_gif_box.volume()))
-print('大礼盒的表面积是%d平方厘米'%big_gif_box.area())
-index=big_gif_box.type.index('大礼盒')
-g_box_num=big_gif_box.countBoxnums(200,index)
-print('200条鱼需要%d只大礼盒'%(g_box_num))
-print(g_box_num)
-print('200条鱼装大礼盒的价值为%d元'%(g_box_num*big_gif_box.price))
-small_gif_box=FishBox(50,20,30)
-small_gif_box.price=500
-small_gif_box.amount=10
-small_gif_box.weight=5
-print('小礼盒的体面积是%d立方厘米'%(small_gif_box.volume()))
-print('小礼盒的表面积是%d平方厘米'%small_gif_box.area())
-index=small_gif_box.type.index('小礼盒')
-print(index)
-small_box_num=big_gif_box.countBoxnums(200,index)
-print(small_box_num)
-print('200条鱼需要%d只大礼盒'%(small_box_num))
-print('200条鱼装小礼盒的价值为%d元'%(small_gif_box.price*small_box_num))
+nums=[17,8,7,2,3,6,1,1,5]
+prices=[10.5,6.2,4.7,7.2,12,15,78.1,10.78,7.92]
+amount=sum(nums)
+print('鱼总数:%d'%(amount))
+total=[x*y for x,y in zip(nums,prices)]
+print(sum(total))
